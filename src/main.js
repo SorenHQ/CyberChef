71f2403e116336ef895fb33bb3eaee42b3e79add
nima khaki <nimakhaki123@gmail.com>
2025-04-27 11:26:12 +0330

    minor changes in main.js file

--- src/main.js
+++ src/main.js
@@ -859,8 +859,8 @@ const app = {
   ZlibInflate,
 };
 
-window.addEventListener("DOMContentLoaded", () => {
+(() => {
   window.app = app;
   window.Buffer = Buffer;
   console.log(window.app);
-});
+})();
